3addb88b57741769872e7297fd2a99cc1f1bbee3
ndzf <ndzf.manifest@gmail.com>
2022-12-27 12:51:29 +0700

    Declarative rendering

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <h1>{{ message }}</h1>
+  <span title="static value">Hover me (without declarative rendering)</span>
+  <br>
+  <span :title="message">Hover me (with declarative rendering)</span>
+  <br>
+  <button @click="changeMessage">Change message</button>
 </template>
+
+<script>
+/**
+ * Declarative rendering: vue extends standard HTML with template syntax that allows us to declaratively 
+ * describe HTML output based on javascript state.
+ * source: https://vuejs.org/guide/introduction.html#what-is-vue
+ */
+export default {
+  data() {
+    return {
+      message: "Declarative rendering",
+    }
+  },
+  methods: {
+    changeMessage(e) {
+      this.message = "Hello world";
+    }
+  }
+}
+</script>
