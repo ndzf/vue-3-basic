2464cd7e72e5097c2b395c0ababb5eaf8d57918b
ndzf <ndzf.manifest@gmail.com>
2022-12-27 12:24:25 +0700

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<script>
+
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      const currentCounter = this.count;
+      (currentCounter <= 0) ? this.count : this.count--;
+    }
+  }
+};
+</script>
+
+<template>
+  <h1>Hello World</h1>
+</template>
+
+<style>
+.app-wrapper {
+  display: flex;
+  justify-content: center;
+}
+
+.container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 1rem;
+}
+
+button.btn {
+  background-color: #6777ef;
+  font-size: 1rem;
+  color: white;
+  padding: .5rem;
+}
+
+p {
+  margin: 0;
+}
+</style>
